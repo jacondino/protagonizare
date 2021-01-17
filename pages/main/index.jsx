eceeb86a5b9e906ac556405378759e359f581963
Matheus Oliveira de Oliveira <oliveiramatheus@nb-002845.cwinet.local>
2021-01-17 18:51:56 -0300

    criando tela main

---
+++ pages/main/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './style.scss'
+import { withRouter } from "next/router";
+
+class MainPage extends React.Component {
+
+  componentDidMount() {
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+          
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+    });
+
+  }
+
+  render() {
+    return (
+      <div className="page-main">
+
+        <section id="welcome" className="welcome" >
+          <div className="section-content">
+            <h1>Que bom ter você por aqui! :)</h1>
+            <p>A Wings é uma plataforma colaborativa que permite que você, mãe jovem, tenha acesso a conteúdos importantes para seu desenvolvimento pessoal e profissional.</p>
+            {/* <div className="button">
+              <a href="/login">começar</a>
+            </div> */}
+          </div>
+        </section>
+
+      </div>
+    );
+  }
+
+}
+
+export default withRouter(MainPage);
