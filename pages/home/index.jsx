4b162902e6983fbcfb042b2760f95b437ea70c83
Matheus Oliveira de Oliveira <oliveiramatheus@nb-002845.cwinet.local>
2021-01-17 15:26:34 -0300

    finalizando conteudo da pagina home

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -22,7 +22,23 @@ class HomePage extends React.Component {
             <p>Após o oitavo mês da gravidez e durante o período da licença-maternidade, a jovem tem o direito de continuar estudando em casa</p>
             <a href="#">Lei Federal 6.202/75.</a> <br/>
           </div>
-          <img src="" alt=""/>
+        </section>
+
+        <section className="assistance" >
+          <div className="section-content">
+            <h1>Encontre o(a) assistente social mais perto de você</h1>
+            <p>A Wings é uma plataforma colaborativa que permite que você, mãe jovem, tenha acesso a conteúdos importantes para seu desenvolvimento pessoal e profissional.</p>
+          </div>
+          <div className="section-image"></div>
+        </section>
+
+        <section className="learn" >
+          <div className="section-image"></div>
+          <div className="section-content">
+            <h1>O que você pode aprender com a Wings?</h1>
+            <p>A Wings é uma plataforma colaborativa que permite que você, mãe jovem, tenha acesso a conteúdos importantes para seu desenvolvimento pessoal e profissional. ESCREVER MELHOR SOBRE ISSAQUI.</p>
+            <button>começar</button>
+          </div>
         </section>
 
 
