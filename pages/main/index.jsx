259350c3ef8395f9e6a45c7b88d7f1881553b29f
Matheus Oliveira de Oliveira <oliveiramatheus@nb-002845.cwinet.local>
2021-01-17 19:00:19 -0300

    finalizando tela main

--- pages/main/index.jsx
+++ pages/main/index.jsx
@@ -26,9 +26,14 @@ class MainPage extends React.Component {
           <div className="section-content">
             <h1>Que bom ter você por aqui! :)</h1>
             <p>A Wings é uma plataforma colaborativa que permite que você, mãe jovem, tenha acesso a conteúdos importantes para seu desenvolvimento pessoal e profissional.</p>
-            {/* <div className="button">
-              <a href="/login">começar</a>
-            </div> */}
+            <div className="buttons">
+              <div style={{ backgroundColor: '#BE00EE'}} className="button btn-assistance">
+                <a href="/assistances">Assistentes sociais</a>
+              </div>
+              <div className="button">
+                <a href="/cursos">Acessar aulas</a>
+              </div>
+            </div>
           </div>
         </section>
 
