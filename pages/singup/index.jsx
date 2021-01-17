016a5e3cc5ed969bcc2e6485515ba0f22e3620ed
Matheus Oliveira de Oliveira <oliveiramatheus@nb-002845.cwinet.local>
2021-01-17 18:40:43 -0300

    criando tela de cadastro

--- pages/singup/index.jsx
+++ pages/singup/index.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import './style.scss'
 import { withRouter } from "next/router";
 
-class LoginPage extends React.Component {
+class SingupPage extends React.Component {
 
   render() {
     return (
-      <div className="page-login">
+      <div className="page-singup">
         <div className="content">
           <h1>Olá! :)</h1>
-          <p>Faça login para acessar a plataforma</p>
+          <p>Cadastre-se para acessar a plataforma</p>
           <form action="">
+            <div className="field">
+              <label for="">Nome</label>
+              <input type="text" placeholder="digite aqui o seu nome" />
+            </div>
             <div className="field">
               <label for="">Seu e-mail</label>
               <input type="email" placeholder="exemplo@email.com" />
@@ -19,12 +23,16 @@ class LoginPage extends React.Component {
               <label for="">Sua senha</label>
               <input type="password" placeholder="pelo menos 8 caracteres" />
             </div>
+            <div className="field">
+              <label for="">Confirme sua senha</label>
+              <input type="password" placeholder="digite novamente" />
+            </div>
             <div className="button">
-              <a href="/main">entrar</a>
+              <a href="/home">entrar</a>
             </div>
             <div className="to-singup">
-              <p>Ainda não tem cadastro?</p>
-              <a href="/singup">Cadastre-se aqui</a>
+              <p>Já possui uma conta?</p>
+              <p>Então faça <a href="/login">login</a></p>
             </div>
           </form>
         </div>
@@ -34,4 +42,4 @@ class LoginPage extends React.Component {
 
 }
 
-export default withRouter(LoginPage);
+export default withRouter(SingupPage);
