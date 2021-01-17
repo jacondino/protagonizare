2bf9f34020cf292a6a6bc65e0084ec3dc5f27bae
Anderson Jacondino <anderson.jacondino15@gmail.com>
2021-01-17 14:20:46 -0300

    adicionando config para imagens
    Signed-off-by: Anderson Jacondino <anderson.jacondino15@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
-const withImages = require('next-images');
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
 
 const nextConfig = {
     webpack(config) {
       config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
+        test: /\.(png|jpg|gif|ico)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[contenthash:8].[ext]',
+          publicPath: '/_next/static',
+          outputPath: 'static',
+          esModule: false,
+        },
       });
   
       return config;
     }
   };
 
-  module.exports = withImages(withCSS(withSass(nextConfig)))
+  module.exports = withCSS(withSass(nextConfig))
