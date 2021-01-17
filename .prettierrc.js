182143c4df3a1fb4324eef6b5c69d71fff6965b6
Anderson Jacondino <anderson.jacondino15@gmail.com>
2021-01-17 13:21:37 -0300

    Adicionando menu basico no projeto
    Signed-off-by: Anderson Jacondino <anderson.jacondino15@gmail.com>

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports.getRules = () => {
+    return module.exports = {
+    printWidth: 120,
+    tabWidth: 2,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+  }
+  }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import bar from '../../../public/bars.png'
-import './style.scss'
+import React, { useState } from "react";
+import Link from "next/link";
+import bar from "../../../public/bars.png";
+import close from "../../../public/close.png";
+import "./style.scss";
 
-export const Header = () => (
+export const Header = () => {
+  const [sidebar, setSidebar] = useState(false);
 
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
     <div className="header">
-        <button>
-            <img src={bar} alt='next'/>
+      <div className="navbar">
+        <button className="menu-bars" onClick={showSidebar}>
+          <img src={bar} />
         </button>
+        <span className="logo">WINGS</span>
+        <span className="user">MW</span>
+      </div>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+          <li className="navbar-hrefggle">
+            <button className="menu-bars" onClick={showSidebar}>
+              <img src={close} width="18" />
+            </button>
+          </li>
+          <li className={"nav-text"}>
+            <Link href={"/categorias"}>
+              <a>
+                <span>Cursos</span>
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-)
+  );
+};
